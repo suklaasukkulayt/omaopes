c2881514ca226d5bfadbf2763713f4f6e387b1bf
suklaasukkulayt <suklaasukkulayt@gmail.com>
2024-10-02 19:28:31 +0300

    dotenv lisätty ja aloitettu yhdistys openAI:lle

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express()
 const port = 3000;
@@ -7,15 +10,20 @@ const port = 3000;
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-
 app.use(express.static('public'));
 app.post('/chat', async(req,res) =>{
     const question = req.body.question;
     console.log(question);
 
-    try{
-
-        
+    try{    
+        const response = await fetch('https://api.openai.com/v1/chat/completions',{
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+        },
+        body: JSON.stringify({})
+        });
     }catch(error){
         
     }
